6fdf13e22ed0223bc80ab8d63d88b00fc52f55d9
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2024-04-27 22:42:14 +0300

    mongoose schema

---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  name: String,
+  passwordHash: String,
+})
+
+userSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+module.exports = mongoose.model("User", userSchema)
