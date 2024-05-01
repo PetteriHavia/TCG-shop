0c8485ce0a272693f73e0c586ca5fbc69866593b
Petteri Havia <petsku15@hotmail.fi>
2024-05-01 19:35:57 +0300

    New error message for code E11000 duplicate key error

--- server/utils/middleware.js
+++ server/utils/middleware.js
@@ -7,6 +7,9 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).json({ error: 'Malformatted id' });
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message });
+  } else if (error.name === "MongoServerError" && error.code === 11000) {
+    //const fieldName = Object.keys(error.keyPattern)[0];
+    return response.status(400).json({ error: `Name already exists` });
   } else if (error.name === 'JsonWebTokenError') {
     return response.status(400).json({ error: 'Token missing or invalid' });
   } else if (error.name === 'TokenExpiredError') {
