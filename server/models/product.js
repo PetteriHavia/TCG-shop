6b4f63ace45bf4b98c19d7e079fff237ab8502d0
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2024-05-02 23:15:24 +0300

    Add new field "slug" for more user friendly parameters

--- server/models/product.js
+++ server/models/product.js
@@ -23,6 +23,9 @@ const productSchema = mongoose.Schema({
   image: {
     type: String,
   },
+  slug: {
+    type: String,
+  },
   price: {
     type: mongoose.Schema.Types.Mixed,
     validate: {
