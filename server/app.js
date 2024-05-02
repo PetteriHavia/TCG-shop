166c5cc914da52a01d4ed2df6bed4c60b5d3cb92
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2024-05-03 02:01:58 +0300

    require user and login router

--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ const config = require("./utils/config")
 const logger = require("./utils/logger")
 const categoryRouter = require("./controllers/category")
 const productRouter = require("./controllers/product")
+const userRouter = require("./controllers/user")
 const middleware = require("./utils/middleware")
+const loginRouter = require("./controllers/login")
 
 mongoose.set("strictQuery", false);
 
@@ -20,6 +22,8 @@ mongoose.connect(config.MONGO_URI)
 app.use(express.json());
 app.use("/api/categories", categoryRouter);
 app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter)
 app.use(middleware.errorHandler);
 
 module.exports = app;
