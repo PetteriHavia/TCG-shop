2bca58b09f39acfa30ea4d67f53e5ca0c8aa7a1e
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2024-05-14 01:29:15 +0300

    grouping category data for menu building

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -2,34 +2,63 @@ import { MdSearch } from "react-icons/md";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { useState } from "react";
 import { IconContext } from "react-icons/lib";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { useEffect } from "react";
+import api from "../services/api";
 
 const Navigation = () => {
   const [toggle, setToggle] = useState(false)
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    api.getCategories()
+      .then((data) => (
+        setCategories(data)))
+  }, [])
+
+  const groupedCategories = categories.reduce((groups, category) => {
+    const groupKey = category.group
+    if (!groups[groupKey]) {
+      groups[groupKey] = []
+    }
+    groups[groupKey].push(category)
+    return groups
+  }, {})
+
+  /* Testing multiple ways of implementing the menu
+  
+    const createMenu = Object.keys(groupedCategories).map(groupKey => ({
+      group: groupKey,
+      test: groupedCategories[groupKey].map(category => ({
+        name: category.name,
+        link: `/products/${category.id}`
+      }))
+    }))
+  */
+
 
   return (
     <nav>
-      <ul>
-        <li>
-          <img src="#" alt="Logo" />
-        </li>
-        <li>Pokemon</li>
-        <li>Single Cards</li>
-        <li>Supplies</li>
-        <li>Contact</li>
-      </ul>
-      <div className="nav-actions">
-        <div className="nav-search">
-          <div className={`search-bar ${toggle ? "search-active" : ""}`}>
-            {toggle ? <input type="text" placeholder="Search product..." /> : null}
-            <IconContext.Provider value={{ className: "nav-icon" }}>
+      <IconContext.Provider value={{ size: "2rem" }} >
+        <ul>
+          <li>
+            <img src="#" alt="Logo" />
+          </li>
+          <li>Pokemon <MdKeyboardArrowDown /></li>
+          <li>Single Cards<MdKeyboardArrowDown /></li>
+          <li>Supplies</li>
+          <li>Contact</li>
+        </ul>
+        <div className="nav-actions">
+          <div className="nav-search">
+            <div className={`search-bar ${toggle ? "search-active" : ""}`}>
+              {toggle ? <input type="text" placeholder="Search product..." /> : null}
               <MdSearch onClick={() => setToggle(!toggle)} />
-            </IconContext.Provider>
+            </div>
           </div>
-        </div>
-        <IconContext.Provider value={{ className: "nav-icon" }}>
           <MdOutlineShoppingCart />
-        </IconContext.Provider>
-      </div>
+        </div>
+      </IconContext.Provider>
     </nav >
   )
 }
