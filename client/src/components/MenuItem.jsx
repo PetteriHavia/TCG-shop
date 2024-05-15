b989bded864ffb196937ab040688ccc2bc8c07e0
Petteri Havia <petsku15@hotmail.fi>
2024-05-15 18:12:47 +0300

    working on menu + dropdown

---
+++ client/src/components/MenuItem.jsx
@@ -0,0 +1,31 @@
+import { NavLink, Link } from "react-router-dom"
+
+const MenuItem = ({ item }) => {
+  console.log(item)
+  return (
+    <li>
+      <div className="nav-item-content">
+        <NavLink
+          to="#"
+          className="main-link"
+        >
+          {item.label}
+        </NavLink>
+      </div>
+      {item.children &&
+        <div className="dropdown">
+          {item.children?.map((children, index) => (
+            <div key={index} className="dropdown-item">
+              <p>{children.heading}</p>
+              {children.submenu?.map((sub, index) => (
+                <Link key={index} className="sub-link">{sub.label}</Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      }
+    </li>
+  );
+};
+
+export default MenuItem
