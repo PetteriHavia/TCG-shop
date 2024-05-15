6e9ec1e4ec2f1be4d8f7c44d73c45a8b78ab7483
Petteri Havia <petsku15@hotmail.fi>
2024-05-15 21:40:39 +0300

    icon for menu-item that has submenu

--- client/src/components/MenuItem.jsx
+++ client/src/components/MenuItem.jsx
@@ -1,27 +1,27 @@
 import { NavLink, Link } from "react-router-dom"
+import { MdKeyboardArrowDown } from "react-icons/md";
 
 const MenuItem = ({ item }) => {
   console.log(item)
   return (
     <li>
       <div className="nav-item-content">
-        <NavLink
-          to="#"
-          className="main-link"
-        >
-          {item.label}
+        <NavLink to="#" className="main-link">
+          {item.label} {item.children && <MdKeyboardArrowDown size="25px" />}
         </NavLink>
       </div>
       {item.children &&
-        <div className="dropdown">
-          {item.children?.map((children, index) => (
-            <div key={index} className="dropdown-item">
-              <p>{children.heading}</p>
-              {children.submenu?.map((sub, index) => (
-                <Link key={index} className="sub-link">{sub.label}</Link>
-              ))}
-            </div>
-          ))}
+        <div className="dropdown-container">
+          <div className="dropdown">
+            {item.children?.map((children, index) => (
+              <div key={index} className="dropdown-item">
+                <p>{children.heading}</p>
+                {children.submenu?.map((sub, index) => (
+                  <Link key={index} className="sub-link">{sub.label}</Link>
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
       }
     </li>
