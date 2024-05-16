d4be926547bae1b7ac911714cd1d28b9ae0ab009
Petteri Havia <petsku15@hotmail.fi>
2024-05-16 19:13:36 +0300

    add rtk query for data fetching

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 import categoriesReducer from "../redux/reducers/categoriesReducer"
+import { shopApi } from "./reducers/apiSlice"
 
 export default configureStore({
   reducer: {
     categories: categoriesReducer,
-  }
+    [shopApi.reducerPath]: shopApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(shopApi.middleware)
 })
