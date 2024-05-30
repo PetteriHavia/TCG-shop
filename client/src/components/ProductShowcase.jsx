26e46ce35e350290ba647fd180e62d993ae39a34
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2024-05-30 21:01:15 +0300

    refactor: clean code, change getDiscountPrice and getPrice function location to utils folder

--- client/src/components/ProductShowcase.jsx
+++ client/src/components/ProductShowcase.jsx
@@ -1,23 +1,9 @@
 
 import placeholderIMG from '../assets/images/products-header.png'
 import { Link } from 'react-router-dom'
+import { getDiscountedPrice, getPrice } from '../utils/calculateDicount'
 
 const ProductShowcase = ({ products, header }) => {
-  const getPrice = (product) => {
-    if (Array.isArray(product.price)) {
-      return product.price.length > 0 ? product.price[0].price : null
-    }
-    return product.price
-  }
-
-  const formatPrice = (price) => {
-    return price.toFixed(2)
-  }
-
-  const getDicountedPrice = (price, discount) => {
-    const discountedPrice = price - (discount * price / 100)
-    return formatPrice(discountedPrice)
-  }
 
   return (
     <section>
@@ -32,9 +18,9 @@ const ProductShowcase = ({ products, header }) => {
               {item.discount > 0 ? <p className="discount-sticker">-{item.discount}%</p> : null}
               <img src={placeholderIMG} alt="product image" />
               <div className="product-details">
-                <Link to={`products/${item.productName}`}><p>{item.productName}</p></Link>
+                <Link to={`products/${item.productName.toLowerCase()}`}><p>{item.productName}</p></Link>
                 {item.discount > 0 ?
-                  <h4><span>{getPrice(item)} €</span>{getDicountedPrice(getPrice(item), item.discount)} €</h4>
+                  <h4><span>{getPrice(item)} €</span>{getDiscountedPrice(getPrice(item), item.discount)} €</h4>
                   :
                   <h4>{getPrice(item)} €</h4>
                 }
