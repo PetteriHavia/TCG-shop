12d89c23ebf3a636b7a1f65d4b7d419eb0833a3d
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2024-06-27 23:26:42 +0300

    feat:  Setting up reduct state and reducers for filter system

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit"
 import categoriesReducer from "../redux/reducers/categoriesReducer"
-import cartReducer from "../redux//reducers/cartReducer"
+import cartReducer from "../redux/reducers/cartReducer"
 import { shopApi } from "./reducers/apiSlice"
+import filterReducer from "../redux/reducers/filterReducer"
 
 export default configureStore({
   reducer: {
     categories: categoriesReducer,
     cart: cartReducer,
+    filters: filterReducer,
     [shopApi.reducerPath]: shopApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
