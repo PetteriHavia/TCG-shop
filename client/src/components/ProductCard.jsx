23fc87de9d0faf340130daaf3f61bdb279c74dd2
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2024-07-17 23:35:41 +0300

    style: minor updates

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,27 +1,28 @@
 import placeholderIMG from "../assets/images/products-header.png";
 import { getDiscountedPrice, getPrice } from "../utils/calculateDicount";
 import { Link } from "react-router-dom"
-import { formatPath } from "../utils/formatPath";
 
 const ProductCard = ({ item }) => {
 
   return (
     <>
       {item &&
-        <div key={item._id} className="card">
-          {item.discount > 0 ? <p className="discount-sticker">-{item.discount}%</p> : null}
-          <Link to={`/products/${item.setName}/${item.slug}`} className='product-details'>
+        <Link to={`/products/${item.setName}/${item.slug}`} className='product-details'>
+          <div key={item._id} className="card">
+            {item.discount > 0 ? <p className="discount-sticker">-{item.discount}%</p> : null}
+
             <img src={placeholderIMG} alt="product image" />
             <p>{item.productName}</p>
-          </Link>
-          <div className="product-details">
-            {item.discount > 0 ?
-              <h4><span className="discount-span">{getPrice(item)} €</span>{getDiscountedPrice(getPrice(item), item.discount)} €</h4>
-              :
-              <h4>{getPrice(item)} €</h4>
-            }
+
+            <div className="product-details">
+              {item.discount > 0 ?
+                <h4><span className="discount-span">{getPrice(item)} €</span>{getDiscountedPrice(getPrice(item), item.discount)} €</h4>
+                :
+                <h4>{getPrice(item)} €</h4>
+              }
+            </div>
           </div>
-        </div>
+        </Link>
       }
     </>
   )
